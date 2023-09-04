502a44a1c75c8a8c9811aded856da61d2f212737
SrdTibo <boncybee@gmail.com>
2023-09-04 14:51:19 +0200

    test

---
+++ Module00/ex01/PhoneBook.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/01 15:21:01 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/01 15:21:01 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#ifndef SAMPLE_CLASS_H
+# define SAMPLE_CLASS_H
+
+class	PhoneBook
+{
+	public:
+
+		PhoneBook(void);
+		~PhoneBook(void);
+};
+
+#endif
