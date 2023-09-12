2f144658a6b76efbceb11e2c0e316cafd96ea91a
SrdTibo <boncybee@gmail.com>
2023-09-12 12:12:17 +0200

    cpp

---
+++ Module01/ex00/Zombie.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 11:41:53 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/12 11:41:53 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(void)
+{
+    std::cout << "Constructor called" << std::endl;
+}
+
+Zombie::~Zombie(void)
+{
+    std::cout << this->_name << "died" << std::endl;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << this->_name << ": BraiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ Module01/ex00/Zombie.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 11:34:18 by marvin            #+#    #+#             */
+/*   Updated: 2023/09/12 11:34:18 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_CLASS_H
+# define ZOMBIE_CLASS_H
+#include "Contact.hpp"
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+
+class	Zombie
+{
+	public:
+		Zombie(void);
+		~Zombie(void);
+
+        void announce(void);
+    
+    private:
+        std::string _name;
+};
+Zombie* newZombie(std::string name);
+void    randomChump(std::string name);
+#endif
