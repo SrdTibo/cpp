9884dc629b14cf40296035acdb399e91f8bd6b61
SrdTibo <boncybee@gmail.com>
2023-09-14 17:18:41 +0200

    cpp

--- Module01/ex04/file.cpp
+++ Module01/ex04/file.cpp
@@ -15,27 +15,48 @@
 
 File::File(void)
 {
-    std::cout << CYN << "File constructor called" << NC << std::endl;
+	return;
 }
 
 File::~File(void)
 {
-    std::cout << RED << "File destructor called" << NC << std::endl;
+    return;
 }
 
 int File::modifying(std::string filename, std::string s1, std::string s2)
 {
 	std::string file1;
-	(void)s1;
-	(void)s2;
+	std::string file2;
+	std::size_t found = 0;
 	const char* convertfile = filename.c_str();
-	std::ifstream ifs (convertfile, std::ifstream::in);
+	std::ifstream ifs (convertfile);
+	std::ofstream ofl((filename + ".replace").c_str());
+	if (ifs.is_open() && ofl.is_open())
+		std::cout << GRN << "Files opened" << file1 << NC << std::endl;
+	else
+	{
+		std::cout << RED << "Error while opening files" << file1 << NC << std::endl;
+		ifs.close();
+		ofl.close();
+		return 1;
+	}
 	char c = ifs.get();
-	while (ifs.good()) {
+	while (ifs.good())
+	{
 		file1 += c;
 		c = ifs.get();
 	}
+	std::cout << CYN << filename << " = "<< file1 << NC;
+	found = file1.find(s1);
+	while (found!=std::string::npos)
+	{
+		file1.erase(found, s1.size());
+		file1.insert(found, s2);
+		found = file1.find(s1);
+	}
+	ofl << file1;
+	std::cout << CYN << filename << ".replace = "<< file1 << NC;
 	ifs.close();
-	
+	ofl.close();
 	return 0;
 }
--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -15,13 +15,33 @@
 
 int main(int argc, char **argv)
 {
+	std::string av2 = argv[2];
+	std::string av3 = argv[3];
+	std::string av1 = argv[1];
+	if (av1 == "file")
+	{
+		std::cout << RED << \
+		"WTF Don't open the compiled file"\
+		<< NC <<std::endl;
+		return 1;
+	}
 	if (argc == 4)
 	{
-		File file1;
-		if (file1.modifying(argv[1], argv[2], argv[3]) == 1)
+		if (av2 == av3)
+		{
+			std::cout << RED << \
+			"S1 and S2 need to be different : ./file <filename> <s1> <s2>"\
+			<< NC <<std::endl;
 			return 1;
+		}
+		else
+		{
+			File file1;
+			if (file1.modifying(argv[1], argv[2], argv[3]) == 1)
+				return 1;
+		}
 	}
 	else
 		std::cout << RED << \
-		"Only 3 arguments needed : ./file <filename> <s1> <s2>" << NC <<std::endl;
+		"3 arguments needed : ./file <filename> <s1> <s2>" << NC <<std::endl;
 }
